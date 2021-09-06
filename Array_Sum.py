5fdaebe47d3bf689f8787553f585b88e141efc17
Aaron <69317200+Aaronphilip2003@users.noreply.github.com>
2021-09-06 10:43:38 +0530

    Create Array_Sum.py

---
+++ Array_Sum.py
@@ -0,0 +1,11 @@
+!pip install nose
+def pair_sum(arr,k):
+    counter = 0
+    lookup = set()
+    for num in arr:
+        if k-num in lookup:
+            counter+=1
+        else:
+            lookup.add(num)
+    return counter
+    pass
